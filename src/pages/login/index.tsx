4b2ac4192d50b6db17abb6260b81e288a97ef16c
Patrick Rocha <patrick.rochapereira@hotmail.com>
2024-10-19 08:20:43 -0300

    update

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,5 @@
 import { Link, useNavigate } from "react-router-dom";
-import logoImg from "../../assets/logo.svg";
+import logoImg from "../../assets/logo.png";
 import { Container } from "../../components/container";
 import { Input } from "../../components/input";
 import { useEffect } from "react";
